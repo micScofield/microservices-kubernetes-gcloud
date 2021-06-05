d00eb8fcfaf62ee032c0053424066af85b58241d
Sanyam Jain <sanyamjaino98@gmail.com>
2021-06-05 16:28:28 +0530

    Finished error handling

--- auth/src/errors/not-found-error.ts
+++ auth/src/errors/not-found-error.ts
@@ -1,14 +1,14 @@
 import { CustomError } from './custom-error'
 
-export class DatabaseConnectionError extends CustomError {
+export class NotFoundError extends CustomError {
     statusCode = 404
     reason = 'Route not found'
 
     constructor() {
-        super('Route not found - 404')
+        super('Route not found 404')
 
         //only because of typescript, we need to specify that we are extending a built in class
-        Object.setPrototypeOf(this, DatabaseConnectionError.prototype) 
+        Object.setPrototypeOf(this, NotFoundError.prototype) 
     }
 
     serializeErrors = () => ([{ message: this.reason }])
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -12,7 +12,7 @@ router.post(
         body('email').isEmail().withMessage('Invalid Email !'),
         body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
     ],
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             throw new RequestValidationError(errors.array())
