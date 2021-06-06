1ce36aeb55870e02dc3701ec17143fbff97db5f3
Sanyam Jain <sanyamjaino98@gmail.com>
2021-06-06 16:34:13 +0530

    Added test dependencies

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,6 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
 
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
-import { NotFoundError } from './errors/not-found-error'
-import { errorHandler } from './middlewares/error-handler'
-
-const app = express()
-app.set('trust proxy', true)
-app.use(json())
-
-app.use(cookieSession({
-    signed: false, //no encryption on cookie
-    secure: true //send over https connection only
-}))
-
-// User Routes
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
-
-// 404
-app.all('*', async (req, res, next) => {
-    throw new NotFoundError()
-})
-
-app.use(errorHandler)
+import { app } from './app'
 
 const start = async () => {
 
