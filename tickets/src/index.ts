5c21a19bbaf8e1d44617a15b959c0e3b4d34171f
Sanyam Jain <sanyamjaino98@gmail.com>
2021-06-11 23:58:36 +0530

    Graceful shutdown

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -17,6 +17,14 @@ const start = async () => {
         await natsWrapper.connect('ticketing', 'client_id', 'http://nats-srv:4222') 
         //cluster id is the one which we gave inside nats-depl file
 
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed!')
+            process.exit()
+        })
+
+        process.on('SIGINT', () => natsWrapper.client.close())
+        process.on('SIGTERM', () => natsWrapper.client.close())
+
         await mongoose.connect(
             process.env.MONGO_URI, 
             { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
