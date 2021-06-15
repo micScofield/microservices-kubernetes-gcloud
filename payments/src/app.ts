6efbfa07dcd9f090007cae8abf4c81d3f0ab184c
Sanyam Jain <sanyamjaino98@gmail.com>
2021-06-16 03:39:15 +0530

    Setting up payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+import { NotFoundError, errorHandler, currentUser } from '@jainsanyam/common'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(json())
+
+app.use(cookieSession({
+    signed: false, //no encryption on cookie
+    secure: process.env.NODE_ENV !== 'test' //send over https connection only
+}))
+
+app.use(currentUser);
+
+// 404
+app.all('*', async (req, res, next) => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
